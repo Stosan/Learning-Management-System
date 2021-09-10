575c4be597d8f633b31f77d9dac5861864f5e844
Ayo Tosan Samuel <46663234+Stosan@users.noreply.github.com>
2021-09-10 01:37:11 +0100

    Update settings.py

--- lms/settings.py
+++ lms/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-*6m0104-$wfq7a8#n0xdznaq+6ep!ww(x!1i-1msl@nbarej%)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
